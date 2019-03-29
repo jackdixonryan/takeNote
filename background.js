e608be6d75c4e6f1ea2af2003cdd423837c268c7
Jack Ryan <jack.dixon.ryan@gmail.com>
2019-03-29 08:23:58 -0600

    Got firebase working. It was all because I stole a bad CSP from an incorrect manifest.

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({color: "#3aa757"}, () => {
     console.log('the color is green.');
--- popup.js
+++ popup.js
@@ -1,8 +1,23 @@
 
-let inputValue = document.getElementById('page-title-input');
-let addDetails = document.getElementById('add-details');
-let notesBox = document.getElementById('notes-box');
-let takeNote = document.getElementById('take-note');
+var config = {
+  apiKey: "AIzaSyCw_DDfKRwiS7QTqOdvTaHx_zILpDFpLuw",
+  authDomain: "takenote-b375b.firebaseapp.com",
+  databaseURL: "https://takenote-b375b.firebaseio.com",
+  projectId: "takenote-b375b",
+  storageBucket: "",
+  messagingSenderId: "978893534539"
+};
+
+firebase.initializeApp(config);
+
+const db = firebase.firestore();
+const notesRef = db.collection('notes');
+
+const inputValue = document.getElementById('page-title-input');
+const addDetails = document.getElementById('add-details');
+const notesBox = document.getElementById('notes-box');
+const takeNote = document.getElementById('take-note');
+const throwError = document.getElementById('error');
 
 let url;
 
@@ -15,19 +30,21 @@ chrome.tabs.query({
 });
 
 addDetails.addEventListener('click', () => {
-  let textArea = `
+  const textArea = `
     <textarea class="form-control" id="userNotes" style="margin-bottom: 1rem;"></textarea>
   `;
   notesBox.innerHTML = textArea;
 });
 
 takeNote.addEventListener('click', () => {
-  let savePackage = {
-    textAreaValue: document.getElementById('userNotes').value,
+  const userCommentArea = document.getElementById('userNotes');
+  console.log(userCommentArea);
+  const userAddedComments = userCommentArea ? userCommentArea.value : null;
+
+  notesRef.add({
     url: url,
     title: inputValue.value,
     date: new Date,
-  }
-
-  console.log(savePackage);
+    userComments: userAddedComments,
+  })
 });
