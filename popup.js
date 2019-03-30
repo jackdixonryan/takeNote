66f1096d6588a6a3651b1656bbcbd41124aa7750
Jack Ryan <jack.dixon.ryan@gmail.com>
2019-03-30 08:29:56 -0600

    There are some sizing issues to address but in the last forty minutes I added a list of links with page titles and comments (dates to come) and tab navigation between the note form and the recent visits list. Starting to resemble an MVP.

--- popup.js
+++ popup.js
@@ -19,6 +19,16 @@ const notesBox = document.getElementById('notes-box');
 const takeNote = document.getElementById('take-note');
 const throwError = document.getElementById('error');
 
+const mostRecent = document.getElementById('most-recent');
+mostRecent.hidden = true;
+
+const mainForm = document.getElementById('main-form');
+
+const listGroup = document.getElementById('visited-links');
+
+const navLinks = document.getElementsByClassName('nav-link');
+console.log(navLinks);
+
 let url;
 
 chrome.tabs.query({
@@ -28,6 +38,35 @@ chrome.tabs.query({
   inputValue.value = tabs[0].title;
   url = tabs[0].url;
 });
+
+db.collection('notes').get()
+  .then(notes => {
+
+    for (let i = 0; i < notes.docs.length; i++) {
+      db.collection('notes')
+        .doc(notes.docs[i].id)
+        .get()
+        .then(result => {
+          const noteData = result.data();
+          const newListItem = `
+            <a class="list-group-item list-group-item-action flex-column align-items-start" href="${noteData.url}">
+              <div class="d-flex w-100 justify-content-between">
+                <h6>${noteData.title}</h6>
+                <small>A Date</small>
+              </div>
+              <p class="mb-1">${noteData.userComments || 'No comments.'}</p>
+            </a>
+          `;
+          listGroup.innerHTML += newListItem;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  });
 
 addDetails.addEventListener('click', () => {
   const textArea = `
@@ -48,3 +87,23 @@ takeNote.addEventListener('click', () => {
     userComments: userAddedComments,
   })
 });
+
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener('click', () => {
+    // active = i;
+    if (i === 0) {
+      navLinks[0].className = "nav-link active";
+      navLinks[1].className = "nav-link";
+
+      mostRecent.hidden = true;
+      mainForm.hidden = false;
+    } else {
+      navLinks[1].className = "nav-link active";
+      navLinks[0].className = "nav-link";
+
+      mostRecent.hidden = false;
+      mainForm.hidden = true;
+    }
+  });
+}
